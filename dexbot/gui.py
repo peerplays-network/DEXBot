77953e137fd3f446565d9b8fa53a03e6b315e328
Jackodb <jackodeboer@live.nl>
2019-08-13 09:12:39 +0200

    Up to date visualization of mountain mode
    Documentation can be found here: https://github.com/Jackodb/Visualization/issues

--- dexbot/gui.py
+++ dexbot/gui.py
@@ -56,38 +56,29 @@ def main():
 def server():
     """ Server to provide statistical visualization of a worker
     """
-    # Todo: Change to use data from Bitshares
-    df = pd.read_csv('test.csv',
-                     usecols=['Price', 'Mountain_neutral_view', 'Current'])
     # Fixme: Include this file in the project instead of loading from external source
     external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
     dash_app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-    dash_app.layout = html.Div(id='wrapper', style={'height': '80vh', 'width': '100vh'}, children=[
-        # Represents the URL bar, doesn't render anything
-        dcc.Location(id='url', refresh=False),
-
+    dash_app.layout = html.Div(id='wrapper', style={'height': '80vh'}, children=[
         html.H1(children='Staggered Orders Overview'),
         html.H2(children='A mountain based visualisation of (base asset/quote asset)'),
-        html.Div(id='output-figure'),
-        dcc.Input(id='input1', type='text'),  # Price
-        dcc.Input(id='input2', type='text'),  # Order size
-        html.Button(id='submit-button', n_clicks=0, children='Submit')
+        # Fixme: Button's layout is inherited in some bad way because it looks good in browser
+        html.Button(id='submit-button', n_clicks=0, children='Switch'),
+        html.Div(id='output-graph'),
+
+        # Represents the URL bar, doesn't render anything
+        dcc.Location(id='url', refresh=False)
 
     ])
 
-    @dash_app.callback(
-        dash.dependencies.Output('wrapper', 'children'),
-        [dash.dependencies.Input('url', 'pathname')])
-    def display_page(pathname):
-        """ This is called on every page load and URL change.
-            # Todo: this can be changed to only retrieve data of the worker
-            # Todo: Add documentation
-            :param pathname: URL of the worker
-            :return:
-        """
+    @dash_app.callback(Output('output-graph', 'children'),
+                       [Input('submit-button', 'n_clicks'),
+                        Input('url','pathname')])
+    def display_page(n_clicks,pathname):
 
+        # Todo: the if pathname part needs to be moved into another function but that causes errors
         if pathname:
             # Add readable spaces to worker name if there are any
             pathname = pathname.replace("%20", " ")
@@ -100,109 +91,137 @@ def server():
 
             # Create Bitshares Account instance using the account name
             account = Account(account_name)
-            # Todo: Get's all open orders for this account
-            # print(account.openorders)
 
-            # Return updated figure
-            return html.Div(id='wrapper', style={'height': '80vh', 'width': '100vh'}, children=[
-                html.H1(children='Staggered Orders Overview for {}'.format(worker_name)),
-                html.H2(children='A mountain based visualisation of (base asset/quote asset)'),
-                html.Div(id='output-figure'),
-                dcc.Input(id='input1', type='text'),  # Price
-                dcc.Input(id='input2', type='text'),  # Order size
-                html.Button(id='submit-button', n_clicks=0, children='Submit')
+            # Todo: This config stuff needs to be adjusted
+            config = Config()
+            worker_config = config
+            # print(worker_config.workers_data)
 
-            ])
+            worker_market = worker_config.workers_data[worker_name]['market']
+            worker_market = worker_market.split('/')
 
-        # Return empty div
-        return html.Div()
+            base_asset = worker_market[1]
+            quote_asset = worker_market[0]
 
-    @dash_app.callback(Output('output-figure', 'children'),
-                       [Input('submit-button', 'n_clicks')],
-                       [State('input1', 'value'),
-                        State('input2', 'value')])
-    def update_figure(n_clicks, input1, input2):
-        """ Todo: Add documentation
+            df = pd.DataFrame({'price':[],
+                               'order_size':[],
+                               'current_orders':[],
+                               'initial_orders':[],
+                               'bar_colors_current':[],
+                               'bar_colors_initial':[]})
 
-            :param n_clicks:
-            :param input1:
-            :param input2:
-            :return:
-        """
-        bar_background_colors_initial = []
-        bar_background_colors_current = []
 
-        initial_dict = {}
-        current_dict = {}
-        for x, y, z in zip(df.Price, df.Mountain_neutral_view, df.Current):
-            initial_dict[round(x, 3)] = y
-            current_dict[round(x, 3)] = z
+            df, cp = get_orders(df,account,base_asset)
 
-        if input1 is not None:  # NEEDS TO BE ADDED: if not input1 == ''
-            input_price = float(input1)
-            if input_price in list(initial_dict.keys()):
-                current_dict[input_price] += float(input2)
-                initial_dict[input_price] -= float(input2)
-            else:
-                print('error')
+            # Fixme: is it necessary to append this in the Pandas dataframe? If yes, do it in get_orders
+            ticks = [i for i in range(len(df['price'].tolist()))]
+            text = [str(i) for i in df['price'].tolist()]
 
-        for item in initial_dict:
-            item = round(item, 3)
-            if item < 1:
-                bar_background_colors_initial.append('rgba(58, 98, 87, 0.5)')
-                bar_background_colors_current.append('rgba(51, 204, 51, 0.9)')
-            else:
-                if item == 1:
-                    bar_background_colors_initial.append('rgba(217,217,217,0.5)')
-                    bar_background_colors_current.append('rgba(242,242,242,0.9)')
-                else:
-                    bar_background_colors_initial.append('rgba(230, 0, 0, 0.5)')
-                    bar_background_colors_current.append('rgba(255, 0, 0, 0.9)')
+            # Todo: change 'current' stuff once update on db is done
+            current_orders = {} # placeholder so program doesn't crash
+            bar_colors_current = [] # placeholder so program doesn't crash
 
-        print(list(current_dict.values()))
+            # Get half of all orders to determine range for y-axis
+            half = df['order_size'].head(int(len(df['order_size'])*(50/100))).tolist()
 
-        data = [
-            go.Bar(
-                x=[i for i in range(len(list(df.Price)))],  # Length of the dict (len(dict))
-                y=list(current_dict.values()),  # All values from current_dict
-                name='Buy',
-                marker=go.bar.Marker(
-                    color=bar_background_colors_current
-                )
-            ),
-            go.Bar(
-                x=[i for i in range(len(list(df.Price)))],  # Length of the dict
-                y=list(initial_dict.values()),  # All values from mountain_dict
-                name='Sell',
-                marker=go.bar.Marker(
-                    color=bar_background_colors_initial
-                )
-            )
-        ]
+            if n_clicks is 0 or n_clicks % 2 == 0: # check whether the number of clicks is an even number, if so, vertical view
+                figure_dict = {}
+                data = [go.Bar(x=[i for i in range(len(df['price'].tolist()))],
+                               y=list(current_orders.values()),name='Current',
+                               marker=go.bar.Marker(color=bar_colors_current)),
+                        go.Bar(x=[i for i in range(len(df['price'].tolist()))],
+                               y=df['order_size'].tolist(),name='Initial',
+                               marker=go.bar.Marker(color=df['bar_colors_initial'].tolist()),
+                               orientation='v')]
+                figure_dict['data'] = data
+                xaxis_dict = {'title':quote_asset + '/' + base_asset,
+                              'zerolinecolor':'rgba(153,153,153,0.2)','tickvals':ticks,
+                              'ticktext':text,'showticklabels':False}
+                yaxis_dict = {'title':'Order size','gridcolor':'rgba(153,153,153,0.2)',
+                              'range':[half[0],half[-1]]}
 
+                figure = get_figure(figure_dict,xaxis_dict,yaxis_dict)
+
+                return figure
+
+            elif n_clicks % 2 != 0: # check whether the number of clicks isn't an even number, if so, horizontal view
+                figure_dict = {}
+                data = [go.Bar(x=list(current_orders.values()),
+                               y=[i for i in range(len(df['price'].tolist()))],name='Current',
+                               marker=go.bar.Marker(color=bar_colors_current)),
+                        go.Bar(x=df['order_size'].tolist(),
+                               y=[i for i in range(len(df['price'].tolist()))],name='Initial',
+                               marker=go.bar.Marker(color=df['bar_colors_initial'].tolist()),
+                               orientation='h')]
+                figure_dict['data'] = data
+                xaxis_dict = {'title':'Order size','gridcolor':'rgba(153,153,153,0.2)',
+                              'range':[half[0],half[-1]]}
+                yaxis_dict = {'title':quote_asset + '/' + base_asset,'zerolinecolor':'rgba(153,153,153,0.2)',
+                              'tickvals':ticks,'ticktext':text,'showticklabels':False}
+
+                figure = get_figure(figure_dict,xaxis_dict,yaxis_dict)
+
+                return figure
+
+
+    def get_orders(df,account,base_asset):
+
+        # Todo: create an if-statement to check whether the order is a current_order or initial_order
+        for order in account.openorders:
+            if order['for_sale']['symbol'] == base_asset: # if the asset that you sell is equal to base asset, it's a buy order
+                df = df.append({'price':round(order['price'],3),
+                                'order_size':float(order['base']['amount']*order['price']),
+                                'bar_colors_initial':'rgba(58, 98, 87, 0.5)'},ignore_index=True)
+
+            else: # otherwise it's a sell order
+                price = round(float(order['quote']['amount'])/float(order['base']['amount']),3)
+
+                df = df.append({'price':price,'order_size':float(order['base']['amount']*order['price']),
+                                'bar_colors_initial':'rgba(230, 0, 0, 0.5)'},ignore_index=True)
+
+        # sort values in dataframe by price to get mountain type visualization
+        df = df.sort_values(by=['price']) # print(df.sort_values(by=['price']))
+
+        # Get center price (middle row in dataframe is df.loc[ 0 , : ])
+        # Todo: add center price (market price) + fictional order size which is bigger than highest sell and buy order size
+        cp = df.loc[ 0 , : ]['price']
+
+        # Adjust bar_colors_initial for center price (so it becomes white)
+        df.at[ 0 , 'bar_colors_initial'] = 'rgba(217,217,217,0.5)'
+
+        # Todo: this may be needed in case the update of storing orders in db is done
+        # initial_orders = {}
+        # current_orders = {}
+        # for x, y in zip(price_list, order_size):
+        #     initial_orders[round(x, 3)] = y
+        #     # current_dict[round(x, 3)] = z
+
+        return df, cp
+
+
+    def get_figure(figure_dict,xaxis_dict,yaxis_dict):
+        data = figure_dict['data']
         return dcc.Graph(
             id='my-figure',
             figure=go.Figure(
                 data=data,
                 layout=go.Layout(
                     autosize=True,
-                    xaxis=dict(
-                        title='BTS/USD',
-                        zerolinecolor='rgba(153,153,153,0.2)'
-                    ),
-                    yaxis=dict(
-                        title='Order size',
-                        gridcolor='rgba(153,153,153,0.2)'
-                    ),
+                    xaxis=xaxis_dict,
+                    yaxis=yaxis_dict,
                     barmode='stack',
                     plot_bgcolor='rgb(21,43,42)',
                     paper_bgcolor='rgb(21,43,42)',
                     font={
                         'color': 'white'
-                    }
+                    },
+                    showlegend=True
                 )
             ),
-            style={'height': '100%', 'width': '60vw', 'display': 'inline-block'}
+            config={
+                'displayModeBar': False
+            },
+            style={'height': '100%', 'width': '80vw', 'display': 'inline-block'}
         )
 
     dash_app.run_server()
